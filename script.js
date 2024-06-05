eae5a274b535d6c6c2b088f3420dd1a04153c97c
asimk <asimkm786@outlook.com>
2024-06-05 11:57:56 -0400

    Added RainbowMode, changed color of outer border and fixed logic for click event

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
 const container = document.querySelector(".container");
 const rowContainer = document.querySelector(".row-container");
-const createGrid = (size) =>
+let isRainbowMode = false;
+function createGrid(size)
     {
         for (i = 0; i < size; i++)
         {
                 const row = document.createElement("div");
+                console.log(3);
                 row.classList.add("row");
                 rowContainer.appendChild(row);
-                console.log("1");
                 for (j = 0; j < size; j++) {
                     const square = document.createElement("div");
                     square.classList.add("grid-square");
                     row.appendChild(square);
-                    console.log("2");
                 }    
         }
     };
+const clearGrid = () => {
+    while(rowContainer.firstChild) {
+        rowContainer.removeChild(rowContainer.firstChild);
+    }
+}
+const buildRgbVal = (r, b, g) => {
+    return "rgb(" + r + "," + g + "," + b +")";
+}
 createGrid(16);
+addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("grid-square"))
+        {
+            if (isRainbowMode) {
+                rgb1 = Math.floor(Math.random() * 256);
+                rgb2 = Math.floor(Math.random() * 256);
+                rgb3 = Math.floor(Math.random() * 256);
+                rgb = buildRgbVal(rgb1, rgb2, rgb3);
+                event.target.style.backgroundColor = rgb;
+            } else 
+            event.target.style.backgroundColor = "black";
+
+        }
+});
+addEventListener("click", (event) => {
+    if (event.target.classList.contains("grid-change"))
+        {
+            const size = prompt("Choose size:");
+            clearGrid();
+            createGrid(size);
+        }
+    if (event.target.classList.contains("rainbow-mode")) { 
+        isRainbowMode = !isRainbowMode;
+    }
+});
